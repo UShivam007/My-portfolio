bfccd1029963b62b37287ddc83863b6fc322e5b9
UShivam007 <ushivam19@gmail.com>
2026-08-28 13:51:39 +0530

    Finalized

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     return (
         <div id="home" className='hero'>
             <img src={profile_img} alt="" />
-            <h1><span>I am Shivam Upadhyay,</span><br /> Full Stack Developer Based in Bhopal <font color='#f08804'>(M.P)</font> .</h1>
+            <h1><span>I am Shiivam Upadhyay,</span><br /> Full Stack Developer Based in Bhopal <font color='#f08804'>(M.P)</font> .</h1>
             <p>I'm a Full Stack Developer, From Bhopal with 2yr of Experience in Java Application Development.J2EE </p>
             <div className="hero-action">
                 <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href="#contact">Connect With Me </AnchorLink></div>
